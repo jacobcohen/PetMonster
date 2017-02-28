c817e299754022e3c63821027b7a0231d8484952
Jake Cohen <jacob.cohen327@gmail.com>
2017-02-28 16:29:51 -0500

    feat(routes) users routes created and moved it to a new folder: routes

--- server/auth.filters.js
+++ server/auth.filters.js
@@ -7,6 +7,13 @@ const mustBeLoggedIn = (req, res, next) => {
   next()
 }
 
+// const mustBeAdmin = (req, res, next) => {
+//   if (!req.user.isAdmin) {
+//     return res.status(401).send('You must be an admin')
+//   }
+//   next()
+// }
+
 const selfOnly = action => (req, res, next) => {
   if (req.params.id !== req.user.id) {
     return res.status(403).send(`You can only ${action} yourself.`)
