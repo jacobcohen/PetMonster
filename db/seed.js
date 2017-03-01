28909abea3dcb4c580320937d350f12513b98082
Jonathan Fuchs <jgfuchs@gmail.com>
2017-03-01 11:49:23 -0500

    feat(backend routes and models)seeded products and set api/products path

--- db/seed.js
+++ db/seed.js
@@ -9,9 +9,16 @@ const seedUsers = () => db.Promise.map([
   {firstName: 'Professional', lastName: 'Man', email: 'admin@hotmail.com', isAdmin: true, streetAddress: '13 fake Street', city: 'New Bork', state: 'AA', zipCode: 12345, creditCard: 4321567390123458, cvc: 719}
 ], user => db.model('users').create(user))
 
+const seedProducts = () => db.Promise.map([
+  {name: 'The Mummy', imageURLs: ['https://s-media-cache-ak0.pinimg.com/originals/08/e7/93/08e7938cbbf8c8a6102798e439585773.jpg'], price: 300.00, description: 'Mummy issues? Look no further! This monster will slowly shamble towards you. Like a zombie, but from Egypt. Exotic!', stock: 1},
+  {name: 'The Wolfman', imageURLs: ['https://marruda3.files.wordpress.com/2013/10/the-wolf-man.jpg'], price: 200.00, description: "A dog is man's best friend, so this wolfman is his own best friend. Self-cleaning. May or may not fetch your slippers", stock: 1}
+], product => db.model('products').create(product))
+
 db.didSync
-  .then(() => db.sync({force: false}))
+  .then(() => db.sync({force: true}))
   .then(seedUsers)
   .then(users => console.log(`Seeded ${users.length} users OK`))
+  .then(seedProducts)
+  .then(products => console.log(`Seeded ${products.length} products OK`))
   .catch(error => console.error(error))
   .finally(() => db.close())
