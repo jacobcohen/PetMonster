fff14957372abfcad3987d54c92ca9c722e17584
Jonathan Fuchs <jgfuchs@gmail.com>
2017-03-01 17:50:52 -0500

    feat(routes) order route started

--- server/routes/orders.js
+++ server/routes/orders.js
@@ -5,3 +5,16 @@ const Transaction = db.model('transactions')
 const Order = db.model('orders')
 const Product = db.model('products')
 
+module.exports = require('express').Router() // eslint-disable-line new-cap
+  .get('/all', (req, res, next) =>
+    Order.findAll()
+    .then(orders => res.json(orders))
+    .catch(next))
+  .get('/:orderId', (req, res, next) =>
+    Order.findById(req.params.orderId)
+    .then(order => res.json(order))
+    .catch(next))
+  .get('/userOrders/:userId', (req, res, next) =>
+    Order.findById(req.params.orderId)
+    .then(order => res.json(order))
+    .catch(next))
