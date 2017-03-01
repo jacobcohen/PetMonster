d3a89bdb142d7791bad79c5d352bc6ac711c1900
Jonathan Fuchs <jgfuchs@gmail.com>
2017-03-01 14:01:54 -0500

    Cooler branch (#20)
    * feat(routes) users routes created and moved it to a new folder: routes
    
    * bug(backend - sequelize)-cannot get any data from database through router
    
    * bugfix(backend - sequelize) figured out that db was overwriting seed file
    
    * feat(user model)added validation for several columns
    
    * fix(backend routes)fixed user put route
    
    * feat(backend routes and models)seeded products and set api/products path
    
    * feat(routes) added reviews routes, updated seed file for reviews, commented out route for accessing all reviews for a product within product route file (now in review route file)

--- db/seed.js
+++ db/seed.js
@@ -9,9 +9,24 @@ const seedUsers = () => db.Promise.map([
   {firstName: 'Professional', lastName: 'Man', email: 'admin@hotmail.com', isAdmin: true, streetAddress: '13 fake Street', city: 'New Bork', state: 'AA', zipCode: 12345, creditCard: 4321567390123458, cvc: 719}
 ], user => db.model('users').create(user))
 
+const seedProducts = () => db.Promise.map([
+  {name: 'The Mummy', imageURLs: ['https://s-media-cache-ak0.pinimg.com/originals/08/e7/93/08e7938cbbf8c8a6102798e439585773.jpg'], price: 300.00, description: 'Mummy issues? Look no further! This monster will slowly shamble towards you. Like a zombie, but from Egypt. Exotic!', stock: 1},
+  {name: 'The Wolfman', imageURLs: ['https://marruda3.files.wordpress.com/2013/10/the-wolf-man.jpg'], price: 200.00, description: "A dog is man's best friend, so this wolfman is his own best friend. Self-cleaning. May or may not fetch your slippers", stock: 1}
+], product => db.model('products').create(product))
+
+const seedReviews = () => db.Promise.map([
+  {rating: 5, description: "it was the best monster I've ever seen. would monster again", user_id: 1, product_id: 1},
+  {rating: 3, description: "it was only an ok monster. kinda hokey, would want something more modern", user_id: 1, product_id: 2},
+  {rating: 1, description: "this was the worst Angie's List contractor I've ever seen. I wanted him to remodel my kitchen, and he was a monster!", user_id: 2, product_id: 1}
+], review => db.model('reviews').create(review))
+
 db.didSync
   .then(() => db.sync({force: false}))
   .then(seedUsers)
   .then(users => console.log(`Seeded ${users.length} users OK`))
+  .then(seedProducts)
+  .then(products => console.log(`Seeded ${products.length} products OK`))
+  .then(seedReviews)
+  .then(reviews => console.log(`Seeded ${reviews.length} reviews OK`))
   .catch(error => console.error(error))
   .finally(() => db.close())
---
+++ server/routes/reviews.js
@@ -0,0 +1,52 @@
+'use strict'; // eslint-disable-line semi
+
+const db = require('APP/db')
+const Review = db.model('reviews')
+const User = db.model('users') 
+const Product = db.model('products')
+
+module.exports = require('express').Router() // eslint-disable-line new-cap
+  .get('/', (req, res, next) =>
+    Review.findAll()
+    .then(reviews => res.json(reviews))
+    .catch(next))
+  .post('/:userId/:prodId', (req, res, next) =>
+    {   
+      const userPromise = User.findById(req.params.userId).catch(next),
+            prodPromise = Product.findById(req.params.prodId).catch(next)
+      
+      Promise.all([userPromise, prodPromise])
+      .then(([user, prod]) => {return user.addProductReviews(prod, req.body)})
+      .then(([review]) => res.status(201).json(review))
+      .catch(next)
+    })
+    //return user.addProductReviews(product, { rating: 5, description: 'it was the best' });
+     
+  .param('reviewId', (req, res, next, reviewId) =>
+    Review.findById(req.params.reviewId)
+    .then(review => {
+      if (!review) {
+        next(new Error('failed to load review'));
+      } else {
+        req.review = review
+        next()
+      }
+    })
+    .catch(next))
+  .get('/:reviewId', (req, res, next) => {
+    res.send(req.review)
+  })
+  .get('/byUser/:userId', (req, res, next) =>
+    Review.findAll({where: {user_id: req.params.userId}})
+    //req.review.getReviews()
+    .then(reviews => res.json(reviews))
+    .catch(next))
+  .get('/byProd/:prodId', (req, res, next) =>
+    Review.findAll({where: {product_id: req.params.prodId}})
+    //req.review.getReviews()
+    .then(reviews => res.json(reviews))
+    .catch(next))
+  .delete('/:reviewId', (req, res, next) => //must put back in mustBeLoggedIn
+    User.destroy({where: {id: req.params.reviewId}})
+      .then(user => res.json(user))
+      .catch(next))
