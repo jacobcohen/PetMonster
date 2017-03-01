5f304b12a4c7c3a1f2dc847991dc7dd43317a9a2
Jonathan Fuchs <jgfuchs@gmail.com>
2017-03-01 11:11:22 -0500

    Cooler branch (#19)
    * feat(routes) users routes created and moved it to a new folder: routes
    
    * bug(backend - sequelize)-cannot get any data from database through router
    
    * bugfix(backend - sequelize) figured out that db was overwriting seed file
    
    * feat(user model)added validation for several columns

---
+++ server/routes/users.js
@@ -0,0 +1,34 @@
+'use strict'; // eslint-disable-line semi
+
+const db = require('APP/db')
+const User = db.model('users')
+
+const {mustBeLoggedIn, forbidden} = require('../auth.filters')
+
+module.exports = require('express').Router() // eslint-disable-line new-cap
+  .get('/', (req, res, next) => //add back forbidden('only admins can list users'),
+  {
+     User.findAll()
+      .then(users => {
+        console.log(users)
+        res.json(users)
+        console.log('hello')
+      })
+      .catch(next)
+  })
+  .post('/', (req, res, next) =>
+    User.create(req.body)
+      .then(user => res.status(201).json(user))
+      .catch(next))
+  .get('/:id', (req, res, next) => //must put back in mustBeLoggedIn -- don't forget!
+    User.findById(req.params.id)
+      .then(user => res.json(user))
+      .catch(next))
+  .put('/:id', (req, res, next) =>  //maybe add mustBeAdmin?
+    User.update(req.body, {where: {id:req.params.id}})
+      .then(user => res.json(user))
+      .catch(next))
+  .delete('/:id', (req, res, next) => //must put back in mustBeLoggedIn
+    User.destroy({where: {id: req.params.id}})
+      .then(user => res.json(user))
+      .catch(next))
