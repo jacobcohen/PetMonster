e641cf5c2b4e460f2c9949fa3301e54852a66eaa
Maria Xia <maria.xia@gmail.com>
2017-03-02 18:59:50 -0500

    feat(Order model): new instance method for emptying cart, feat(Product): two new instance methods for adding pics and selecting default pic, test(Product & Order): new tests

--- db/models/product.js
+++ db/models/product.js
@@ -6,11 +6,29 @@ const db = require('APP/db')
 
 const Product = db.define('products', {
     name: { type: Sequelize.STRING, allowNull: false },
-    imageURLs: { type: Sequelize.ARRAY(Sequelize.STRING), defaultValue: ['http://www.illuminessensce.com/wp-content/uploads/2012/12/Image-Coming-Soon-Placeholder.png'] },
-    price: { type: Sequelize.DECIMAL(10, 2), allowNull: false },
+    imageURLs: { 
+        type: Sequelize.ARRAY(Sequelize.STRING), 
+        defaultValue: ['http://www.illuminessensce.com/wp-content/uploads/2012/12/Image-Coming-Soon-Placeholder.png'] 
+    },
+    price: { type: Sequelize.INTEGER, allowNull: false },
     description: { type: Sequelize.TEXT, allowNull: false },
     stock: { type: Sequelize.INTEGER, allowNull: false }
 }, {
+    instanceMethods: {
+        addPicture: function(url){
+            this.imageURLs.push(url)
+            return this.save()
+        },
+        setDefaultPicture: function(indexOfPicture){
+            if (indexOfPicture >= this.imageURLs.length) {
+                return Promise.reject(new Error('Invalid picture index.'))
+            } else {
+                let [newDefault] = this.imageURLs.splice(indexOfPicture, 1)
+                this.imageURLs.unshift(newDefault)
+                return this.save()
+            }            
+        }
+    },
     /**
      * Scope for in-stock products
      */
