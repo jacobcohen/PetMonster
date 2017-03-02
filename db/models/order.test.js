2622c1572b9b672271626f2892c836c4fb78938a
Maria <maria.xia@gmail.com>
2017-03-02 12:24:12 -0500

    Branchacho (#24)
    * feat(orders and transactions) & test(orders and transactions)
    
    * fix(test(order.test.js)): using beforeEach and afterEach
    
    * feat(order model): updates selling price when order placed
    
    * trying to add instance methods to the order model
    
    * fix(order) & test(order): addToCart and purchase instance methods on order model, specs passing
    
    * docs(models): comments for models
    
    * docs(models): more comments for models
    
    * feat(order model): updateCart instance method takes productId parameter, NOT the product
    
    * feat(order model): updateCart takes integer params, no longer objects

--- db/models/order.test.js
+++ db/models/order.test.js
@@ -16,33 +16,31 @@ const expect = chai.expect
 describe('Order model', () => {
   before('wait for the db', () => db.didSync)
 
-  let user, product, order, transaction
-  before(function() {
+  let user, cookieMonster
+  before(function() { // seed the db for all tests
 
-    const productPromise = Product.create({
+    const creatingCookieMonster = Product.create({
       name: 'CookieMonster',
       price: 200.00,
       description: 'I love cookies.',
       stock: 5
     })
-    const userPromise = User.create({
+    const creatingUser = User.create({
       firstName: 'Maria',
       lastName: 'X',
       email: 'mx@gmail.com'
     })
-    const orderPromise = Order.create({})
+    
 
-    return Promise.all([userPromise, productPromise, orderPromise])
-    .then(([createdUser, createdProduct, createdOrder]) => {
-        user = createdUser
-        product = createdProduct
-        order = createdOrder
-        return user.addOrder(order)
+    return Promise.all([creatingUser, creatingCookieMonster])
+    .then(([newUser, newMonster]) => {
+        user = newUser
+        cookieMonster = newMonster
     })
     .catch(console.error)
   })
 
-  after(function(){
+  after(function(){ // clean up after all tests
       return Promise.all([
           Product.truncate({cascade: true}),
           User.truncate({cascade: true}),
@@ -52,6 +50,21 @@ describe('Order model', () => {
   })
 
   describe('newly created order', () => {
+
+    let order
+    before('create an order', () => {
+        return Order.create({})
+        .then(createdOrder => {
+            order = createdOrder
+            return user.addOrder(order)
+        })
+        .catch(console.error)
+    })
+
+    after(() => {
+        order.destroy()
+    })
+
     it('has fields we expect', () => {
         expect(order.total).to.equal('0.00')
         expect(order.status).to.equal('active')
@@ -66,20 +79,30 @@ describe('Order model', () => {
 
   describe('adding a transaction to an active order (cart)', () => {
 
-    before('add product to cart', () => {
-        return order.updateCart(product.id, { quantity: 3 })
+    let order, transaction
+    before('create an order', () => {
+        return Order.create({})
+        .then(createdOrder => {
+            order = createdOrder
+            return user.addOrder(order)
+        })
+        .then(updatedUser => order.updateCart(cookieMonster.id, 2))
         .then(([createdTransaction]) => {
             transaction = createdTransaction[0]
         })
         .catch(console.error)
     })
 
+    after(() => {
+        order.destroy()
+    })
+
     describe('adding a new product', () => {
         it('should create a transaction in the transaction pivot table', () => {
-            expect(transaction.quantity).to.equal(3)
+            expect(transaction.quantity).to.equal(2)
         })
         it('runs the beforeUpdate hook on the order, which updates the total', () => {
-            expect(order.total).to.equal(600)
+            expect(order.total).to.equal(400)
         })
         it('associates the transaction to the order', () => {
             return order.getProducts().then((products) => {
@@ -90,9 +113,9 @@ describe('Order model', () => {
 
     describe('increasing the quantity of a product you already have', () => {
         it('should update the transaction quantity', () => {
-            return order.updateCart(product.id, {quantity: 1})
+            return order.updateCart(cookieMonster.id, 1)
                 .then(updatedTransaction => {
-                    expect(updatedTransaction.quantity).to.equal(4)
+                    expect(updatedTransaction.quantity).to.equal(1)
                 })
                 .catch(console.error)
         })
