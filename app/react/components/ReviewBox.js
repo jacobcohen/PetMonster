a604782a4e3bb02b1b2cc5b73f96390c35d02fca
Jake Cohen <jacob.cohen327@gmail.com>
2017-03-07 10:57:39 -0500

    deleting bad reviews component, moved reviews stuff to correct folder

--- app/react/components/ReviewBox.js
+++ app/react/components/ReviewBox.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import {connect} from 'react-redux'
 import axios from 'axios'
-import {checkValidReviewer} from '../reducers/reviews.js'
+import {checkValidReviewer} from '../../reducers/reviews.js'
 
 class ReviewBox extends Component {
 
